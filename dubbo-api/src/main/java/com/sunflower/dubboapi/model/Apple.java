9c09b433b39e555572f88a6b931df13fb1bd1030
kesonghe <rd318@netxue.com.cn>
2019-08-08 13:50:35 +0800

    新增苹果类

---
+++ dubbo-api/src/main/java/com/sunflower/dubboapi/model/Apple.java
@@ -0,0 +1,13 @@
+package com.sunflower.dubboapi.model;
+
+/**
+ * @program: springboot_dubbo
+ * @description: 苹果类
+ * @author: chenzhennan
+ * @create: 2019-08-08 13:49
+ **/
+public class Apple {
+    public static void main(String[] args) {
+        System.out.println("我是小苹果！！");
+    }
+}
